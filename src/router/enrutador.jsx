7ec72cb3c16c2ade743a86b8fa36c42e31f80d4c
esteban <teban@gmail.com>
2025-04-01 10:23:33 -0500

    feat se crea el enrutador

---
+++ src/router/enrutador.jsx
@@ -0,0 +1,12 @@
+import Home from '../Home'
+import Login from '../pages/Login'
+export let enrutador = [
+    {
+        path: '/',
+        element: <Login/>
+    },
+    {
+        path: '/home',
+        element: <Home/>
+    }
+]
